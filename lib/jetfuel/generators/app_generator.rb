15cbbe6f01de0d98e60f0dcfacea76f7cd2b8fea
Ben Roesch <bcroesch@gmail.com>
2014-07-11 14:58:38 -0500

    Remove dev prime task

--- lib/jetfuel/generators/app_generator.rb
+++ lib/jetfuel/generators/app_generator.rb
@@ -77,7 +77,6 @@ module Jetfuel
       build :raise_on_delivery_errors
       build :raise_on_unpermitted_parameters
       build :provide_setup_script
-      build :provide_dev_prime_task
       build :configure_generators
     end
 
