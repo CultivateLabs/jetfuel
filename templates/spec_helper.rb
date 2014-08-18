5efa8bb6b4ca4eeae1dabe38c051c4e344bf5952
David Van Der Beek <earlynovrock@gmail.com>
2014-08-18 12:27:51 -0500

    add pry-byebug and infer spec type from file location

--- templates/spec_helper.rb
+++ templates/spec_helper.rb
@@ -21,6 +21,7 @@ RSpec.configure do |config|
   config.include Devise::TestHelpers, type: :controller
 
   config.infer_base_class_for_anonymous_controllers = false
+  config.infer_spec_type_from_file_location!
   config.order = 'random'
   config.use_transactional_fixtures = false
 end
